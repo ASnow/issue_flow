fd14dd0886dca156fff29e5841f207c79b6026c9
Андрей Большов <bolshov@uniweb.ru>
2016-01-29 19:15:40 +0300

    init daemon

---
+++ lib/daemons/warden.rb
@@ -0,0 +1,84 @@
+begin
+  require 'daemons'
+rescue LoadError
+  raise "You need to add gem 'daemons' to your Gemfile if you wish to use it."
+end
+require 'optparse'
+
+# 
+# Control class for Directory-server.
+# Parse arguments, configure and start server
+module Daemons
+  class Warden
+    PROCESS_NAME = 'warden_manager'
+    # 
+    # Constructor. Parse arguments passed from console
+    # @param args [Array] console arguments ARGV
+    # 
+    # @return [Directory::Command] object of class
+    def initialize(args)
+      @options = {
+        :quiet => true,
+        :pid_dir => "#{Rails.root}/tmp/pids",
+        :address => '127.0.0.1',
+        :port => 8080
+      }
+
+      @monitor = false
+
+      opts = OptionParser.new do |opt|
+        opt.banner = "Usage: directory [options] start|stop|restart|run"
+
+        opt.on('-a', '--address=127.0.0.1', 'Host to bind server') do |address|
+          @options[:address] = address
+        end
+        opt.on('-p', '--port=31337', 'Port to bind server') do |n|
+          @options[:port] = n.to_i
+        end
+        opt.on_tail('-h', '--help', 'Show this message') do
+          puts opt
+          exit 1
+        end
+      end
+      @args = opts.parse!(args)
+    end
+
+    # 
+    # Daemonize of server
+    def daemonize
+      dir = @options[:pid_dir]
+      Dir.mkdir(dir) unless File.exist?(dir)
+      run_process(PROCESS_NAME, @options)
+    end
+
+    # 
+    # Run process
+    # @param process_name [String] name of process
+    # @param options [Hash] options
+    def run_process(process_name, options = {})
+      Daemons.run_proc(process_name,  :dir => options[:pid_dir],
+                                      :dir_mode => :normal, 
+                                      :monitor => @monitor, 
+                                      :ARGV => @args) do |*_args|
+        $0 = File.join(options[:prefix], process_name) if @options[:prefix]
+        run process_name, options
+      end
+    end
+
+    # 
+    # Start EventMachine
+    # @param worker_name [String] name of process
+    # @param options [Hash] options
+    def run(worker_name = nil, options = {})
+      Dir.chdir(Rails.root)
+
+      Rails.logger = Logger.new(File.join(Rails.root, 'log', "#{PROCESS_NAME}.log"))
+      ::Warden::Server.start options
+    rescue => e
+      Rails.logger.fatal e
+      STDERR.puts e.message
+      exit 1
+    end
+    
+  end
+end
